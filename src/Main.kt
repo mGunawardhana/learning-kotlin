29e8d7f70ac390980484131a9d122110ef16dcee
mGunawardhana <mrgunawardhana27368@gmail.com>
2024-10-04 22:17:21 +0530

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,11 @@
+
+fun main() {
+    val userName:String = "Maneesha"
+//    userName = "Maneesha" // Error: Val cannot be reassigned
+
+    var userAge:Int = 25
+    userAge = 26 // No Error: Var can be reassigned
+
+    println("User Name: $userName")
+    println("User Age: $userAge")
+}
